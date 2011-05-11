60acbf319aac92a2e5942e5ccb52e01ea207eb62
Linus Oleander <linus@oleander.nu>
2011-05-11 22:02:08 +0200

    Adding VCR

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,7 @@
 require "rspec"
 require "webmock/rspec"
 require "undertexter"
+require "vcr"
 require "subtitle"
 require "undertexter/error"
 
@@ -8,4 +9,14 @@ WebMock.allow_net_connect!
 
 RSpec.configure do |config|
   config.mock_with :rspec
+  config.extend VCR::RSpec::Macros
+end
+
+VCR.config do |c|
+  c.cassette_library_dir = "spec/fixtures/vcr_cassettes"
+  c.stub_with :webmock
+  c.default_cassette_options = {
+    record: :all
+  }
+  c.allow_http_connections_when_no_cassette = false
 end
--- undertexter.gemspec
+++ undertexter.gemspec
@@ -25,4 +25,5 @@ Gem::Specification.new do |s|
   
   s.add_development_dependency("rspec", "2.4.0")
   s.add_development_dependency("webmock")
+  s.add_development_dependency("vcr")
 end
